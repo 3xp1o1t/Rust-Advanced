46ebab5426843bae9cbb6b90e455decc76dc96dc
3xp1o1t <jes.monti.503@gmail.com>
2023-12-26 23:31:12 -0600

    Add communication btw server/client

--- scenario1/tcpserver/src/main.rs
+++ scenario1/tcpserver/src/main.rs
@@ -1,3 +1,17 @@
+use std::io::{Read, Write};
+use std::net::TcpListener;
 fn main() {
-    println!("Hello, world!");
+    let connection_listener = TcpListener::bind("127.0.0.1:3000");
+    println!("Running on port 3000");
+    for stream in connection_listener.unwrap().incoming() {
+        let mut stream = stream.expect("Failed to establish connection");
+        println!("Connection established!");
+        let mut buffer = [0; 1024];
+        stream
+            .read(&mut buffer)
+            .expect("Failed to read data from stream");
+        stream
+            .write(&buffer)
+            .expect("Failed to write data to stream");
+    }
 }
