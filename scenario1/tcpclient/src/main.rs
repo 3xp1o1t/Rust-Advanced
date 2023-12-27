8aa38a4bab1282ad9e8d1d59480a36f59d5a513b
3xp1o1t <jes.monti.503@gmail.com>
2023-12-26 23:57:58 -0600

    Refactor tcpclient/tcpserver

--- scenario1/tcpclient/src/main.rs
+++ scenario1/tcpclient/src/main.rs
@@ -1,4 +1,18 @@
+use std::io::{Read, Write};
 use std::net::TcpStream;
-fn main() {
-    let _stream = TcpStream::connect("localhost:3000").unwrap();
+use std::str;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut stream = TcpStream::connect("localhost:3000")?;
+
+    stream.write("Hola".as_bytes())?;
+
+    let mut buffer = [0; 5];
+
+    stream.read(&mut buffer)?;
+
+    let response_str = str::from_utf8(&buffer)?;
+
+    println!("Got response from server:{:?}", response_str);
+
+    Ok(())
 }
--- scenario1/tcpserver/src/main.rs
+++ scenario1/tcpserver/src/main.rs
@@ -1,17 +1,32 @@
 use std::io::{Read, Write};
-use std::net::TcpListener;
-fn main() {
-    let connection_listener = TcpListener::bind("127.0.0.1:3000");
+use std::net::{TcpListener, TcpStream};
+
+fn handle_client(mut stream: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
+    println!("Connection established");
+
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer)?;
+
+    stream.write_all(&buffer)?;
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("127.0.0.1:3000")?;
     println!("Running on port 3000");
-    for stream in connection_listener.unwrap().incoming() {
-        let mut stream = stream.expect("Failed to establish connection");
-        println!("Connection established!");
-        let mut buffer = [0; 1024];
-        stream
-            .read(&mut buffer)
-            .expect("Failed to read data from stream");
-        stream
-            .write(&buffer)
-            .expect("Failed to write data to stream");
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                if let Err(err) = handle_client(stream) {
+                    eprint!("Error handling client: {}", err);
+                }
+            }
+            Err(err) => {
+                eprint!("Failed to establish connection: {}", err);
+            }
+        }
     }
+
+    Ok(())
 }
