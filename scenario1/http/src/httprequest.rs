932452599043bc507503b6e4218c78a881004c3e
3xp1o1t <jes.monti.503@gmail.com>
2023-12-28 00:41:24 -0600

    add httprequest struct - todo 2 methods

--- scenario1/http/src/httprequest.rs
+++ scenario1/http/src/httprequest.rs
@@ -1,7 +1,66 @@
+use std::collections::HashMap;
+
+#[derive(Debug, PartialEq)]
+pub enum Resource {
+    Path(String),
+}
+
+#[derive(Debug)]
+pub struct HttpRequest {
+    pub method: Method,
+    pub version: Version,
+    pub resource: Resource,
+    pub headers: HashMap<String, String>,
+    pub msg_body: String,
+}
+
+impl From<String> for HttpRequest {
+    fn from(req: String) -> Self {
+        let mut parsed_method = Method::Uninitialized;
+        let mut parsed_version = Version::V1_1;
+        let mut parsed_resource = Resource::Path("".to_string());
+        let mut parsed_headers = HashMap::new();
+        let mut parsed_msg_body = "";
+
+        // Read each line in the incoming HTTP request
+        for line in req.lines() {
+            if line.contains("HTTP") {
+                let (method, resource, version) = process_req_line(line);
+                parsed_method = method;
+                parsed_version = version;
+                parsed_resource = resource;
+            } else if line.contains(":") {
+                let (key, value) = process_header_line(line);
+                parsed_headers.insert(key, value);
+            } else if line.len() == 0 {
+                // If it is blank line, do nothing
+            } else {
+                parsed_msg_body = line;
+            }
+        }
+
+        // Parse the incoming HTTP request into HttpRequest struct
+        HttpRequest {
+            method: parsed_method,
+            version: parsed_version,
+            resource: parsed_resource,
+            headers: parsed_headers,
+            msg_body: parsed_msg_body.to_string(),
+        }
+    }
+}
+
 #[derive(Debug, PartialEq)]
 pub enum Method {
     Get,
     Post,
+    Uninitialized,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Version {
+    V1_1,
+    V2_0,
     Uninitialized,
 }
 
@@ -15,6 +74,15 @@ impl From<&str> for Method {
     }
 }
 
+impl From<&str> for Version {
+    fn from(http_version: &str) -> Version {
+        match http_version {
+            "HTTP/1.1" => Version::V1_1,
+            _ => Version::Uninitialized,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -22,5 +90,11 @@ mod tests {
     fn test_method_into() {
         let method: Method = "GET".into();
         assert_eq!(method, Method::Get);
+    }
+
+    #[test]
+    fn test_version_into() {
+        let http_version: Version = "HTTP/1.1".into();
+        assert_eq!(http_version, Version::V1_1);
     }
 }
